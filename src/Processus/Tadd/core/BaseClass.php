4a907abb4fdb8c48cfe861c98629e5724ef9876e
FrancisVarga <github@varga-net.com>
2012-11-28 12:56:40 +0100

    adding some new stuff

---
+++ src/Processus/Tadd/core/BaseClass.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hissterkiller
+ * Date: 11/28/12
+ * Time: 11:36 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class BaseClass
+{
+
+}
