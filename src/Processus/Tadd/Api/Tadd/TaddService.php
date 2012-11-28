3ddc22c0b72ce6ba0eafb4b06329ca857fb2a7a4
FrancisVarga <github@varga-net.com>
2012-11-28 12:56:19 +0100

    add some stubbed stuff

---
+++ src/Processus/Tadd/Api/Tadd/TaddService.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: hissterkiller
+ * Date: 11/28/12
+ * Time: 11:33 AM
+ * To change this template use File | Settings | File Templates.
+ */
+namespace Processus\Tadd\Api\Tadd;
+
+class TaddService
+{
+
+}
